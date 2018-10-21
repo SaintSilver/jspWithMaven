cec11dff56dafc4cb363e889f905a9b7da8ad153
kutar37 <kutar37@gmail.com>
2018-10-21 12:59:08 +0900

    use lambda expression

--- src/main/java/com/kutar/user/UserDAO.java
+++ src/main/java/com/kutar/user/UserDAO.java
@@ -1,7 +1,5 @@
 package com.kutar.user;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import core.jdbc.JdbcTemplate;
@@ -10,53 +8,32 @@ import core.jdbc.RowMapper;
 public class UserDAO {
 
 	private JdbcTemplate template = new JdbcTemplate();
+	
+	public User findByUserId(String userId) {
+		RowMapper<User> rm = rs -> new User(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4));
+		String sql = "select * from users where userId=?";
+		return template.executeQuery(sql, rm, userId);
+	}
+	
+	public List<User> findUsers() {
+		RowMapper<User> rm = rs -> new User(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4));
+		String sql = "select * from users";
+		return template.executeQueryList(sql, rm);
+	}
 
 	public void addUser(User user) {
-
 		String sql = "insert into users values(?,?,?,?)";
 		template.executeUpdate(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
 	}
 
-	public User findByUserId(String userId) {
-
-		RowMapper<User> rm = new RowMapper<User>() {
-
-			@Override
-			public User mapRow(ResultSet rs) throws SQLException{
-				return new User(
-						rs.getString(1), 
-						rs.getString(2), 
-						rs.getString(3), 
-						rs.getString(4));
-			}
-		};
-
-		String sql = "select * from users where userId=?";
-		return template.executeQuery(sql, rm, userId);
-	}
-
 	public void removeUser(String userId) {
-
 		String sql = "delete from users where userId =?";
 		template.executeUpdate(sql, userId);
 	}
 
 	public void updateUser(User user) {
-
 		String sql = "update users set password=?,name=?,email=? where userId=?";
 		template.executeUpdate(sql, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
 	}
 
-	public List<User> findUsers(){
-		RowMapper<User> rm = new RowMapper<User>() {
-
-			@Override
-			public User mapRow(ResultSet rs) throws SQLException {
-				return new User(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4));
-			}
-		};
-
-		String sql = "select * from users";
-		return template.executeQueryList(sql, rm);
-	}
 }
